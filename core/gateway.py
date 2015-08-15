01c158a140f15c4630dcc8d95476f4ea559357fe
logi <logileifs@gmail.com>
2015-08-15 17:47:07 +0000

    improved exception handling

--- core/gateway.py
+++ core/gateway.py
@@ -32,12 +32,13 @@ def do_payment(route_name, data):
 	try:
 		acq_msg = parser.parse(data)
 		print('acq_msg: ' + acq_msg)
+		#client = route.Client()
+		route1 = route.Route()
+		response = route1.do_payment(acq_msg)
 	except Exception, e:
 		print('could not parse request - ' + str(e))
 		raise e
-	#client = route.Client()
-	route1 = route.Route()
-	response = route1.do_payment(acq_msg)
+
 	print('do_payment returning ' + str(response))
 	return response
 
