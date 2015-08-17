68906dc8b7548fd2ede1c0357085f3b7a87b2817
logi <logileifs@gmail.com>
2015-08-17 21:11:45 +0000

    incoming msg type check, add msg to msg cache and refactoring

--- core/gateway.py
+++ core/gateway.py
@@ -15,6 +15,17 @@ class Gateway(object):
 		super(Gateway, self).__init__()
 		self.arg = arg
 
+	def verify_type(self, msg):
+		if type(msg) is not dict:
+			raise TypeError('Message must be dictionary')
+
+	def get_route_name(self, msg):
+		try:
+			route_name = msg['payment']['route'].lower()
+		except KeyError, e:
+			raise e
+		return route_name
+
 	def load_module(self, module):
 		path = path_to_routes()
 		print('path to routes: ' + path)
@@ -22,47 +33,64 @@ class Gateway(object):
 			print('module: ' + module)
 			m = imp.find_module(module, [path])
 			print('module found: ' + str(m))
-			route = imp.load_module(
-				module,
-				m[0],
-				m[1],
-				('', '', 5))
+			route = imp.load_module(module, m[0], m[1], ('', '', 5))
 			print('route successfully loaded')
-			return route
 		except ImportError, e:
 			raise e
+		return route
 
-	def do_payment(self, route_name, data):
+	def add_to_msg_cache(self, direction, msg):
+		self.msg_cache[direction] = msg
+
+	def do_payment(self, msg):
 		acq_msg = None
 
 		try:
+			#verify that message is a dictionary
+			self.verify_type(msg)
+
+			#get the route name to load from message
+			route_name = self.get_route_name(msg)
+
+			#load the module
 			route = self.load_module(route_name)
-			#pull information from TMS
+
+			#instantiate the route parser
 			parser = route.Parser()
-			acq_msg = parser.parse(data)
+
+			#make sure the message is parsable
+			#get the message so that it can be stored
+			acq_msg = parser.parse(msg)
+
 			print('acq_msg: ' + acq_msg)
-			#client = route.Client()
+
+			#instantiate the route
 			route1 = route.Route()
+
+			self.add_to_msg_cache('IREQ', msg)
+			self.add_to_msg_cache('OREQ', acq_msg)
+
+			#do the payment and get the response back
 			response = route1.do_payment(acq_msg)
+
 		except Exception, e:
-			print('could not parse request - ' + str(e))
 			raise e
 
 		print('do_payment returning ' + str(response))
 		return response
 
-	def do_refund(self, route_name, data):
+	def do_refund(self, msg):
 		#route = loaded(route_name)
 		pass
 
-	def do_void(self, route_name, data):
+	def do_void(self, msg):
 		pass
 
-	def do_authorization(self, route_name, data):
+	def do_authorization(self, msg):
 		pass
 
-	def do_completion(self, route_name, data):
+	def do_completion(self, msg):
 		pass
 
-	def do_cancellation(self, route_name, data):
+	def do_cancellation(self, msg):
 		pass
