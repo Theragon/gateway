f742c99b20667e528250b0f11c8a547d73f803e8
logi <logileifs@gmail.com>
2015-08-17 22:04:55 +0000

    fixed tsys payment test with a valid dictionary

--- core/tests/test_payment.py
+++ core/tests/test_payment.py
@@ -11,32 +11,32 @@ print('2 levels up: ' + os.path.dirname(os.path.dirname(full_path)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(full_path))))
 sys.path.append(proj_root)
 #from core import *
-from core.gateway import *
+from core.gateway import Gateway
 
-payment_json = \
+payment = \
 	{
-		"payment":
+		u"payment":
 		{
 			# required for evo, not for tsys
-			'cardAcceptorId': 'TestHekla5',
-			'paymentScenario': 'CHIP',
-			'softwareVersion': '1.3.1.15',
-			'configVersion': '12',
-			'terminalDateTime': '20130614172956000',
-			'nonce': '7604056809',
-			'authorizationGuid': '86ee88f8-3245-4fa6-a7fb-354ef1813854',
-			'currency': 'GBP',
-			'amount': '10.00',
-			'customerReference': '00000001',
-			'emvData': '4f07a00000000430605712679999',
-			'f22': '51010151114C',
-			'serialNumber': '311001304',
-			'terminalUserId': 'OP1',
-			'deviceType': 'MPED400',
-			'terminalOsVersion': '1.08.00',
-			'transactionCounter': '3',
-			'accountType': '30',
-			'route': 'tsys'
+			u'cardAcceptorId': u'TestHekla5',
+			u'paymentScenario': u'CHIP',
+			u'softwareVersion': u'1.3.1.15',
+			u'configVersion': u'12',
+			u'terminalDateTime': u'20130614172956000',
+			u'nonce': u'7604056809',
+			u'authorizationGuid': u'86ee88f8-3245-4fa6-a7fb-354ef1813854',
+			u'currency': u'GBP',
+			u'amount': u'10.00',
+			u'customerReference': u'00000001',
+			u'emvData': u'4f07a00000000430605712679999',
+			u'f22': u'51010151114C',
+			u'serialNumber': u'311001304',
+			u'terminalUserId': u'OP1',
+			u'deviceType': u'MPED400',
+			u'terminalOsVersion': u'1.08.00',
+			u'transactionCounter': u'3',
+			u'accountType': u'30',
+			u'route': u'tsys'
 		}
 	}
 
@@ -44,16 +44,22 @@ payment_json = \
 class PaymentTests(unittest.TestCase):
 
 	"""docstring"""
+	#gw = None
+
+	@classmethod
+	def setUpClass(cls):
+		cls.gw = Gateway()
 
 	#@unittest.skip("")
 	def test_tsys_payment(self):
-		resp = do_payment('tsys', payment_json)
+		resp = self.gw.do_payment(payment)
 		assert resp is not None
 
+	@unittest.skip("")
 	def test_not_implemented_evo_payment(self):
 		resp = None
 		try:
-			resp = do_payment('evo', payment_json)
+			resp = self.gw.do_payment(payment)
 		except NotImplementedError:
 			#assert isinstance(resp, NotImplementedError)
 			return True
