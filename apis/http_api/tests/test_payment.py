4b0bd96e82d63056cff781d014dc8d7742383481
logi <logileifs@gmail.com>
2015-08-18 23:12:39 +0000

    "unskipped" test xml payment

--- apis/http_api/tests/test_payment.py
+++ apis/http_api/tests/test_payment.py
@@ -46,7 +46,7 @@ class PaymentTests(unittest.TestCase):
 		assert resp.status_code == requests.codes.ok
 		#assert resp.text == 'OK'
 
-	@unittest.skip("")
+	#@unittest.skip("")
 	def test_xml_payment_tsys(self):
 		xml = get_xml_payment('tsys')
 		resp = requests.post(payment_url, data=xml, headers=app_xml)
