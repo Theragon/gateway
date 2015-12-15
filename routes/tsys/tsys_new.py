bec5d45ae34315edda1c881ee658eab8cfd54af7
logi <logantheragon@gmail.com>
2015-12-15 12:54:56 +0000

    handle shutdown gracefully and publish route going down, added create_payment method

--- routes/tsys/tsys_new.py
+++ routes/tsys/tsys_new.py
@@ -1,8 +1,13 @@
+import xmltodict
 import logging
+import random
+import atexit
 import redis
 import json
+import time
 import sys
 from Queue import Queue
+from collections import OrderedDict
 
 from tsysvalidator import validate_msg, ValidationError
 
@@ -24,7 +29,7 @@ class MessageMonitor():
 		s.port = kwargs.get('port', 6379)
 		s.db = kwargs.get('db', 0)
 		s.queue_name = kwargs.get('queue_name', 'tsys:incoming')
-		s.callback = kwargs.get('callback', None)
+		s.callback = kwargs.get('cb', None)
 		if s.callback is None:
 			raise AttributeError('callback must be specified')
 
@@ -37,11 +42,14 @@ class MessageMonitor():
 		return payload
 
 	def wait_for_msg(s):
-		msg = s.red.blpop(s.queue_name, timeout=0)
-		log.info('MessageMonitor delivering msg: ' + msg[1])
-		payload = s.get_payload(msg)
-		return json.loads(payload)
-		#return msg[1]
+		try:
+			msg = s.red.blpop(s.queue_name, timeout=0)
+			log.info('MessageMonitor delivering msg: ' + msg[1])
+			payload = s.get_payload(msg)
+			return json.loads(payload)
+		except KeyboardInterrupt:
+			log.info('shutting down')
+			sys.exit()
 
 	def monitor_msg_queue(s):
 		log.info('MessageMonitor monitoring queue ' + s.queue_name)
@@ -64,6 +72,7 @@ class TsysRoute(object):
 	queue_name = route + ':incoming'
 
 	red = redis.StrictRedis(host='localhost', port=6379, db=0)
+	ps = red.pubsub()
 
 	def msg_received(s, msg):
 		log.info('message received')
@@ -75,22 +84,70 @@ class TsysRoute(object):
 		except ValidationError as e:
 			log.debug('validation failed')
 			log.debug(e.errors)
+			#todo: handle exception correctly
+
 		#todo: create tsys request
+		if txn_type == 'payment':
+			tsys_payment = s.create_payment(msg)
+			log.debug('tsys_payment: ' + tsys_payment)
 		#todo: send to tsys
 		#todo: receive response
 		#todo: create core response
 		#msg = json.loads(msg)
 		guid = msg[txn_type]['guid']
 
+		secs = random.uniform(0, 10)
+		log.info('sleeping for ' + str(secs) + ' seconds')
+		#time.sleep(secs)
+		log.info('done')
 		s.red.rpush(guid, json.dumps(msg))  # echo msg back to http server
+
+	def on_exit(s):
+		s.red.publish('offline_routes', 'tsys')
 
 	def __init__(s):
 		log.info('initializing tsys route')
-		s.msg_mon = MessageMonitor(queue_name=s.queue_name, callback=s.msg_received)
+		s.msg_mon = MessageMonitor(queue_name=s.queue_name, cb=s.msg_received)
+		atexit.register(s.on_exit)
+		s.red.publish('online_routes', 'tsys')
 
 	def start(s):
 		s.msg_mon.monitor_msg_queue()
 		s.msg_received(s.msg_mon.get_msg())
+
+	# move this method to a separate class
+	def create_payment(s, msg):
+		pos_id = "800018160066091"
+		gen_key = "9E08623B129A9C2EF9E01556"
+		dev_id = '002628'
+		ver_id = 'H001'
+
+		payment = OrderedDict()
+		payment['SGREQ'] = {}  # create the nested structure
+
+		payment['SGREQ']['A1'] = pos_id
+		payment['SGREQ']['A3'] = gen_key
+		payment['SGREQ']['A10'] = '10'
+		payment['SGREQ']['A12'] = msg.get('payment').get('paymentCode')
+		payment['SGREQ']['A15'] = msg.get('payment').get('amount')
+		payment['SGREQ']['A17'] = msg.get('payment').get('transactionCounter')
+
+		payment['SGREQ']['A21'] = {}
+		payment['SGREQ']['A21']['B1'] = msg.get('payment').get('f22')[0]
+		payment['SGREQ']['A21']['B2'] = msg.get('payment').get('f22')[1]
+		payment['SGREQ']['A21']['B3'] = msg.get('payment').get('f22')[2]
+		payment['SGREQ']['A21']['B4'] = msg.get('payment').get('f22')[3]
+		payment['SGREQ']['A21']['B5'] = msg.get('payment').get('f22')[4]
+		payment['SGREQ']['A21']['B6'] = msg.get('payment').get('f22')[5]
+		payment['SGREQ']['A21']['B9'] = msg.get('payment').get('f22')[6]
+		payment['SGREQ']['A21']['B13'] = msg.get('payment').get('f22')[7]
+
+		payment['SGREQ']['A24'] = msg.get('payment').get('track2Data')
+		payment['SGREQ']['A41'] = dev_id
+		payment['SGREQ']['A42'] = ver_id
+
+		xml = xmltodict.unparse(payment, full_document=False)
+		return xml
 
 
 if __name__ == '__main__':
