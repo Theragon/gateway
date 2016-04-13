ecf58fd0e06450f784b6d4975c1fe90c347c7ee0
Logi Leifsson <logantheragon@gmail.com>
2016-04-13 19:46:19 +0000

    renamed new gateway module and added increased fault tolerance using redis pubsub

--- core/gateway.py
+++ core/gateway.py
@@ -1,21 +1,23 @@
 import multiprocessing as mpr
-import logging.config
+import threading
 import logging
-#import redis
-#import json
+import pprint
+import redis
+import time
 import sys
 
 sys.path.append('/home/logi/repos/gateway')
-from messagehandler import MessageHandler
-from utils import dbutils as db
 
-#if sys.version_info[0] < 3:
-#	from logutils.queue import QueueHandler, QueueListener
-#else:
-#	from logging.handlers import QueueHandler, QueueListener
-
+from utils import queueutils as q
+#from utils.parseutils import json_to_dict, dict_to_json
+from utils import messageutils as mu
+from utils import pubsub as ps
 
 log = logging.getLogger(__name__)
+
+debug = True
+red = redis.StrictRedis(host='localhost', port=6379, db=0)
+#ps = red.pubsub()
 
 
 def set_up_log():
@@ -30,86 +32,125 @@ def set_up_log():
 	log.setLevel(logging.DEBUG)
 
 
-class Gateway():
-	#red = redis.StrictRedis(host='localhost', port=6379, db=0)
-	ps = db.red.pubsub()
-	#ps.subscribe('routes')
-	routes = []
+class Worker(mpr.Process):
+	"""docstring for Worker"""
+	def __init__(s):
+		mpr.Process.__init__(s)
+		s.queue_online = q.is_available()
 
-	def register_route(self, msg):
-		route = msg.get('data')
-		log.info('route ' + route + ' is online')
-		self.routes.append(route)
-		log.info(self.routes)
+	def run(s):
+		log.info('worker running')
+		while s.queue_online:
+			msg = s.get_request()
+			route = mu.get_route(msg)
+			queue = route + ':incoming'
+			log.info('delivering message to queue ' + queue)
+			#s.deliver_to_route(queue, dict_to_json(msg))
+			s.deliver_to_route(queue, msg)
+			rsp = s.get_response()
+			s.deliver_to_front(rsp)
 
-	def deregister_route(self, msg):
-		route = msg.get('data')
-		log.info('route ' + route + ' is offline')
+	def get_request(s):
+		log.info('waiting for message on queue incoming')
+		msg = q.dequeue_d('incoming')
+		log.info('received msg ' + str(msg))
+		return msg
+
+	def get_response(s):
+		log.info('waiting for response from route')
+		rsp = q.dequeue_d('core:outgoing')
+		log.info('response received from route: ' + str(rsp))
+		return rsp
+
+	def deliver_to_route(s, queue, msg):
+		q.enqueue_d(queue, msg)
+
+	def deliver_to_front(s, msg):
+		guid = mu.get_guid(msg)
+		q.enqueue_d(guid, msg)
+
+
+class GateWay():
+	"""docstring for GateWay"""
+
+	def monitor_routes(s):
+		while True:
+			# check if new routes are offline/online
+			s.check_route_status()
+			time.sleep(0.25)
+
+	def check_route_status(s):
+		status = None
+		rsp = q.dequeue('route-status', 100)
+		if rsp:
+			log.info('route status: ' + rsp)
+			route, status = rsp.split(':')
+		if status == 'online':
+			s.mark_route_online(route)
+		if status == 'offline':
+			s.mark_route_offline(route)
+
+
+	def mark_route_online(s, route):
+		s.routes.append(route)
+		log.info(pprint.pformat(s.routes))
+
+	def mark_route_offline(s, route):
 		try:
-			self.routes.remove(route)
+			s.routes.remove(route)
 		except ValueError:
 			log.info('route ' + route + ' not registered')
 		finally:
-			log.info(self.routes)
+			log.info(pprint.pformat(s.routes))
 
-	def start_process(self):
-		print('Starting ' + mpr.current_process().name)
 
-	def __init__(self, target=None, callback=None):
-		#s = self
+	def ask_online_routes_to_republish(s):
+		log.info('asking online routes to republish status')
+		ps.publish('route-status-publish', 'all:republish')
 
-		self.ps.subscribe(**{'online_routes': self.register_route})
-		self.ps.subscribe(**{'offline_routes': self.deregister_route})
-		self.thread = self.ps.run_in_thread(sleep_time=0.001)
+
+	def start(s):
+		s.ask_online_routes_to_republish()
+		for i in range(s.num_workers):
+			worker = Worker()
+			s.workers.append(worker)
+			worker.start()
+
+	def exit(s):
+		log.info('waiting for workers to finish')
+		for w in s.workers:
+			w.join()
+		s.thread.join()
+
+	def __init__(s):
 		set_up_log()
 		log.debug('starting Gateway')
-		self.callback = callback
+		s.routes = []
+		if debug:
+			s.num_workers = 1
+		else:
+			s.num_workers = 2
+		q.connect()
 
-		self.workers = []
-		self.pool = mpr.Pool(processes=2, initializer=self.start_process,)
+		s.thread = threading.Thread(target=s.monitor_routes, args=())
+		s.thread.daemon = True
+		s.thread.start()
+
+		s.workers = []
 
 
-	def start(self):
-		try:
-			self.monitor_msg_queue()
-		except KeyboardInterrupt:
-			self.exit()
+def main():
+	from os.path import dirname as dir
+	from sys import path
+	print(dir(path[0]))
+	path.append(dir(path[0]))
 
-	def delegate(self, msg):
-		log.info('starting new process')
-		handler = MessageHandler(msg=msg)
-		self.workers.append(handler)
-		handler.start()
-
-	def exit(self):
-		#s = self
-		log.info('waiting for workers to finish')
-		for w in self.workers:
-			w.join()
-		self.thread.stop()
-
-	def monitor_msg_queue(self):
-		while True:
-			msg = self.get_msg()
-			log.info('message received')
-			log.info(msg)
-			self.delegate(msg)
-
-	def get_payload(self, msg):
-		payload = msg[1]
-		return payload.decode('utf-8')
-
-	def get_msg(self):
-		"""
-		Pop message from message queue and return its payload
-		"""
-		#msg = self.red.blpop('incoming', timeout=0)
-		msg = db.get_msg()
-		#payload = self.get_payload(msg)
-		#return json.loads(payload)
-		return msg
+	gw = GateWay()
+	try:
+		gw.start()
+	finally:
+		gw.exit()
 
 
 if __name__ == '__main__':
-	gw = Gateway()
-	gw.start()
+	main()
